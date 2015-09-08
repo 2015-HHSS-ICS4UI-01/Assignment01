aba15a7d9cab6d6b72ca0a66fc812c3b56c22905
kobed6328 <kobed6328@HRH-IC0027337.wrdsb.ca>
2015-09-08 11:16:55 -0400

    Problem2

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -8,5 +11,24 @@
  * @author kobed6328
  */
 public class Problem2 {
-    
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int[] nums = new int[3];
+        for (int i = 0; i < nums.length; i ++)
+        {
+            System.out.print(">> ");
+            nums[i] = s.nextInt();
+        }
+        
+        int product = nums[0]*nums[1]*nums[2];
+        int square = (int)(Math.pow(product, 2));
+        double root = Math.sqrt(product);
+        
+        System.out.println(product);
+        System.out.println(square);
+        System.out.println(root);
+        
+    }
 }
