21cd756a46391f52f4bd8cb2e4cb7eb30d8cc758
morgennebesenschek <morgennebesenschek@172.20.10.8>
2015-09-09 08:52:51 -0400

    Completed Questions 1 and 2, almost complete Question 3. Cannot figure out why Math.ceil is not working.

---
+++ src/Question1.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author morgennebesenschek
+ */
+public class Question1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);   //imports the Scanner
+        System.out.println("What is your name?"); //asks the user for their name
+        String name = input.nextLine(); //set String name to the the name entered by the user
+        System.out.println("Hello " + name + "!");   //greets the user by the name they entered
+    }
+    
+}
---
+++ src/Question2.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author morgennebesenschek
+ */
+public class Question2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        double[] numbers = new double[3];  //initializes numbers as an array
+        
+        System.out.println("Please enter 3 numbers.");  //asks the user to input three numbers
+        
+        for(int i = 0; i < 3; i++)                      //loop runs until completed 3 times
+        {
+            System.out.println("Number " + (i + 1) + ":");    //asks the user for each individual number
+            numbers[i] = input.nextInt();               //to store in the array numbers
+        }
+        
+        double product = numbers[0] * numbers[1] * numbers[2]; //calculates the product of the three numbers
+        double root = Math.sqrt(product); //calculates the square root of the product
+        double square = Math.pow(product,2); //calculates the square of the product
+        
+        
+        System.out.println("Product: " + product);
+        System.out.println("Square Root of Product: " + root);
+        System.out.println("Product Squared: " + square);
+        
+    }
+    
+}
