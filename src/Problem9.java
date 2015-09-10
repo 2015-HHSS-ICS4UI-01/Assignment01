4dfa3ecca19fd713ae60665f1e916f7b2e5e2768
kobed6328 <kobed6328@HRH-IC0027337.wrdsb.ca>
2015-09-10 10:30:23 -0400

    Problem9-updated-updated

--- src/Problem9.java
+++ src/Problem9.java
@@ -15,13 +15,7 @@ public class Problem9 {
     /**
      * @param args the command line arguments
      */
-    /*Write a program to count the total number of characters
-     * in a series of 10 words that you enter, and compute the 
-     * average word length (store the entered words into an array). 
-     * Output the average word length to the console. Remember, you can 
-     * get the length of a String by asking it. If your string was called word, 
-     * you can use word.length() to return the number of characters the String has. 
-     * Name your class Problem9*/
+
     public static void main(String[] args) {
         
         Scanner s = new Scanner(System.in);
