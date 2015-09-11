7223f8d8777c8c96a3f2f95663f78342bb6800a6
paulm6438 <paulm6438@HRH-IC0027320.wrdsb.ca>
2015-09-11 11:06:28 -0400

    initial

---
+++ src/ProblemThree.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author paulm6438
+ */
+public class ProblemThree {
+    
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        
+        
+
+    }  
+}
