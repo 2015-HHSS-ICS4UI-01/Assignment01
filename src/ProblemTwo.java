8e5c98adb9978fe3b036c6c1cebd277fcab4931b
paulm6438 <paulm6438@HRH-IC0027320.wrdsb.ca>
2015-09-11 10:56:12 -0400

    initial

---
+++ src/ProblemTwo.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author paulm6438
+ */
+public class ProblemTwo {
+    
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("please enter 3 whole numbers");
+        
+        int[] myNums = new int [3];
+        for(int i = 0; i < myNums.length; i++)
+            {
+                System.out.print("Number:");
+                myNums[i]=input.nextInt();
+            }
+
+        int multNums = myNums[0]*myNums[1]*myNums[2];
+        System.out.println("The product is: " + multNums);
+        
+        double sqrNums = Math.sqrt(multNums);
+        System.out.printf("The the square of the product is: %.2f \n ", sqrNums);
+    }
+    
+}
