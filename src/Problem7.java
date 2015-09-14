81340d74e3ebdc69a162f600ac3f25058e9571d6
Jonah <Jonah@jonahscomputer>
2015-09-13 20:05:14 -0400

    problems.

---
+++ src/Problem7.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jonah
+ */
+public class Problem7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //6 billion start
+        //increases by 1.4 a year
+        //pop.>10 billion
+        Scanner in = new Scanner(System.in);
+        
+        long pop = 6000000000;
+        int year = 0;
+        long billion = 10000000000;
+        while (true){
+            year++;
+            pop = pop*1.4;
+            if(pop>)
+        }
+        
+    }
+    
+}
