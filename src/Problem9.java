a829584f884fa597dafe523bb2adb98f20e2b7d4
valet8115 <valet8115@HRH-IC0027322.wrdsb.ca>
2015-09-15 11:06:47 -0400

    up to problem 8

---
+++ src/Problem9.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author valet8115
+ */
+public class Problem9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        
+    }
+}
