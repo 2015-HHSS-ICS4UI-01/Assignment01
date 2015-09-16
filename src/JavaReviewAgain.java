eca9f94aff2de22106cb8b075435ef8901010bd0
natalie <natalie@172.21.7.135>
2015-09-16 11:21:59 -0400

    Finished Review (finally)

---
+++ src/JavaReviewAgain.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author natalie
+ */
+public class JavaReviewAgain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Problem1
+        //Creates scanner for system input
+        Scanner input = new Scanner(System.in);
+        
+        //asks for users name
+        System.out.println("What is your name?");
+        
+        //Gets name and stores in variable
+        String name = input.nextLine();
+        
+        //outputs to screen
+        System.out.println("Hello, " + name + "."); 
+    }
+    
+}
