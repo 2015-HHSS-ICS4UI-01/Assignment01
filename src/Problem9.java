d582543d378d940915cc9fcf9df8f8a7408a9aaf
valet8115 <valet8115@HRH-IC0027322.wrdsb.ca>
2015-09-17 10:42:36 -0400

    done, number 8 is broken

--- src/Problem9.java
+++ src/Problem9.java
@@ -18,6 +18,17 @@ public class Problem9 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
+        System.out.println("Please type a word then hit enter: ");
+        int total = 0;
+        int comp = 0;
+        String[] words = new String[10]; 
         
+        for (int i = 0; i < 10; i++) {
+            words[i] = scan.next();
+            total = total + words[i].length();
+        }
+        comp = total/10;
+        
+        System.out.println("The average word length is: " + comp);
     }
 }
