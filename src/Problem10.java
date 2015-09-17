46df2007e8855926ff949ed9de71f0c16387cc59
paulm6438 <paulm6438@HRH-IC0027320.wrdsb.ca>
2015-09-17 11:09:10 -0400

    initial

--- src/Problem10.java
+++ src/Problem10.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -14,5 +17,21 @@ public class Problem10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int i = 0;
+        while(true )
+        {
+            System.out.println("please enter a word");
+            String word = input.nextLine();
+            if(word.equals("exit"))
+            {
+               break; 
+            }
+            i++;
+        }
+        System.out.println("The amount of words you printed were " + i);
+        
+
+        
     }
 }
