4bd20f3aca9323377635816833b6c88e334f5bf8
naylj6470 <naylj6470@HRH-IC0027325.wrdsb.ca>
2015-09-18 10:47:18 -0400

    Commit 2

--- src/Problem9.java
+++ src/Problem9.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -15,7 +18,17 @@ public class Problem9 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        Scanner input = new Scanner(System.in);
+        int num = 0;
         
-        
+        String[] words = new String[10];
+        for(int i = 0; i< words.length; i++)
+        {
+            System.out.println("Please enter an word");
+            String word = input.nextLine();
+            num = word.length() + num;
+            words[i] = word;
+        }
+        System.out.println(num);
     }
 }
