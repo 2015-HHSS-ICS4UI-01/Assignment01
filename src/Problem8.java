1e8f28691f7d681e2514dbf61a94f945748658ff
naylj6470 <naylj6470@HRH-IC0027325.wrdsb.ca>
2015-09-18 10:25:40 -0400

    Commit 2

--- src/Problem8.java
+++ src/Problem8.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 /*
@@ -18,6 +17,26 @@ public class Problem8 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-              
+        Scanner input = new Scanner(System.in);		          
+        System.out.println("Enter the initial ammount");		
+        double initial = input.nextDouble();		
+        System.out.println("Enter the interest rate");		
+        double interest = input.nextDouble();				
+        double balance = initial;
+        double balance2 = initial;
+        int years;
+        int years2;
+        
+        for(years = 0; balance <= initial*2; years++)
+        {
+            balance =(1 + interest)*balance;
+        }
+        for(years2 = 0; balance2 <= 10000000; years2++)
+        {
+            balance2 =(1 + interest)*balance2;
+        }
+         
+        System.out.println("It will take " + years + " years for the money to double"); 
+        System.out.println("It will take " + years2 + " years for the balance to reach 1 million");
     }
 }
