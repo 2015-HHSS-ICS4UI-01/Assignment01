037561d1b6fc10d1f5b78553eb558ef00904005e
kampn2687 <kampn2687@HRH-IC0027343.wrdsb.ca>
2015-09-21 11:15:44 -0400

    yes

---
+++ src/Problem10.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kampn2687
+ */
+public class Problem10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in); 
+        
+    }
+}
