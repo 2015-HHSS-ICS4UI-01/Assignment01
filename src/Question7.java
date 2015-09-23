3e6757ab89d755371a01d2ab4b72c0a7722ae4c5
thomt9963 <thomt9963@HRH-IC0027344.wrdsb.ca>
2015-09-23 11:13:05 -0400

    rarest of pepes

--- src/Question7.java
+++ src/Question7.java
@@ -13,6 +13,13 @@ public class Question7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int year = 1999;
+        double pop = 6.0;
+        double rate = 0.014;
+        while (pop <= 10) {
+            pop = (pop * rate) + pop;
+            year++;
+        }
+        System.out.println("The population will exceed 10 billion in the year " + year + " at the current growth rate.");
     }
 }
