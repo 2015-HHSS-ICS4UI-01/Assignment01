6b0930ac1f68a239fe047b424d138d520d9795b6
halll7908 <halll7908@HRH-IC0027341.wrdsb.ca>
2016-09-08 14:22:52 -0400

    Day 3 progress.

---
+++ src/A1Q10.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author halll7908
+ */
+public class A1Q10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Scanner
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
