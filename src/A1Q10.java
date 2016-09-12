694752e2c3af0d75246654068da0a61346936d11
halll7908 <halll7908@HRH-IC0027316.wrdsb.ca>
2016-09-12 12:44:53 -0400

    Finished Assignment 1.

--- src/A1Q10.java
+++ src/A1Q10.java
@@ -21,5 +21,32 @@ public class A1Q10 {
         // Scanner
         Scanner input = new Scanner(System.in);
         
-    }
+        // input String
+        boolean done = false;
+        
+        // loop of asking for words until "exit" is entered, "words" is string for input
+        while (!done) {
+            System.out.println("Enter as many words as you wish on one line.");
+            String words = input.nextLine();
+            StringBuilder sBuilder = new StringBuilder(words);
+            
+            // word count variable
+            int amt = 0;
+            // go through letters until a space is found
+            for (int i = 0; i < words.length(); i ++){
+                if (sBuilder.charAt(i) == ' ') {
+                    amt = amt + 1;
+                }
+            }
+            
+            // adding 1 to the amount since the last word will not have a space after it
+            System.out.println("You have entered " + (amt + 1) + " words.");
+            
+            // exit progra,
+        if (words.equals("exit")) {
+                 done = true;
+                 break;
+   }
+  }
+ }
 }
